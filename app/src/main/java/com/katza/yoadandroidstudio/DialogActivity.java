0169e1bf54f976854494aa3f9f6af670c1f05c16
Eyal <eybraL@gmail.com>
2025-12-14 16:46:20 +0200

    dialog

--- app/src/main/java/com/katza/yoadandroidstudio/DialogActivity.java
+++ app/src/main/java/com/katza/yoadandroidstudio/DialogActivity.java
@@ -3,6 +3,7 @@ package com.katza.yoadandroidstudio;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -29,9 +30,31 @@ public class DialogActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { createLoginDialog();}
+        });
+        sp=getSharedPreferences("details1",0);
     }
+    public void createLoginDialog(){
+        d = new Dialog(this);
+        d.setContentView(R.layout.custom_layout);
+        d.setTitle("Login");
+        d.setCancelable(true);
+        etUserName=(EditText) d.findViewById(R.id.etUserName);
+        etPass=(EditText) d.findViewById(R.id.etPassword);
+        btnCustomLogin=(Button) d.findViewById(R.id.btnDialogLogin);
+        btnCustomLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        d.show();
+    }
+
 
 
 
